15f6f2fec53a4bb81961beb5043f48a4c92e299a
sreekanthv1995 <sreekanthv1995@gmail.com>
2025-01-22 17:13:08 +0530

    first commit

---
+++ src/main/java/com/app/scheduler/sheduler/ShedulerApplication.java
@@ -0,0 +1,13 @@
+package com.app.scheduler.sheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/scheduler/sheduler/ShedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.scheduler.sheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
